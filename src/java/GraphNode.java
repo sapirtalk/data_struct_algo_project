f40822b45b612d7cc8c5489f17a8c46adf1a6d34
sapirtalk <70822441+sapirtalk@users.noreply.github.com>
2021-11-26 12:24:02 +0200

    aaa

---
+++ src/java/GraphNode.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class GraphNode {
+    private GraphNode parent = null;
+    private ArrayList<GraphNode> childs = new ArrayList<GraphNode>();
+    private int nodeKey;
+
+    public GraphNode(GraphNode parent , ){
+
+    }
+}
